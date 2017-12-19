942978240846bb710e540d19dd24e9156a83647c
Piotr Ferens <piotrferens@outlook.com>
2017-12-19 22:48:03 +0100

    styles, buttons

--- hackerhunt-client/src/App.js
+++ hackerhunt-client/src/App.js
@@ -1,12 +1,57 @@
 import React, { Component } from "react";
 import glamorous from "glamorous";
-import { Route } from "react-router-dom";
 
-const Title = glamorous.a({
+const PostsWrapper = glamorous.div({
+  display: "flex",
+  flexDirection: "column",
+  flex: 5
+});
+
+const Post = glamorous.article({
+  margin: "auto",
+  width: 500
+});
+
+const MainDiv = glamorous.div({
+  display: "flex",
+  justifyContent: "center"
+});
+
+const Title = glamorous.h2({
   fontSize: 20,
   textDecoration: "none",
   color: "black",
   fontWeight: "bold"
+});
+
+const Menu = glamorous.div({
+  flex: 2,
+
+  justifyContent: "flex-end",
+  textAlign: "left"
+});
+
+const Commercial = glamorous.div({
+  flex: 3
+});
+
+const Top = glamorous.div({
+  display: "flex"
+});
+
+const Logo = glamorous.div({
+  flex: 2
+});
+
+const DivBottomButton = glamorous.div({
+  margin: "auto"
+});
+
+const BottomButton = glamorous.button({
+  backgroundColor: "white",
+  border: "1px solid black",
+  textAlign: "center",
+  width: "100px"
 });
 
 const Description = glamorous.p({});
@@ -20,6 +65,18 @@ const HourDate = glamorous.p({});
 const Comments = glamorous.p({});
 
 const Tags = glamorous.p({});
+
+const Input = glamorous.input({
+  margin: "auto",
+  height: "25px",
+  flex: 5
+});
+
+const ShowMore = glamorous.strong({
+  margin: "auto",
+  marginBottom: 15,
+  textAlign: "center"
+});
 
 class App extends Component {
   state = {
@@ -49,37 +106,66 @@ class App extends Component {
     const filteredPosts = this.state.posts.filter(post =>
       post.title.toLowerCase().includes(this.state.searchText.toLowerCase())
     );
-    const tabPosts = filteredPosts.map(post => post);
+
     return (
       <div>
-        <input onChange={this.onSearch} value={this.searchText} />
-        <Posts tabPosts={tabPosts} />
+        <Top>
+          <Logo>LOGO</Logo>
+          <Input onChange={this.onSearch} value={this.searchText} />
+          <div style={{ flex: 3 }} />
+          <div />
+        </Top>
+        <MainDiv>
+          <Menu>MENU</Menu>
+          <Posts tabPosts={filteredPosts} />
+          <Commercial>COMMERCIAL</Commercial>
+        </MainDiv>
       </div>
     );
   }
 }
 
 class Posts extends Component {
+  state = {
+    showingAll: false
+  };
+
+  showMore = () => {
+    this.setState({ showingAll: true });
+  };
+
   render() {
+    const { tabPosts } = this.props;
     return (
-      <div>
-        {this.props.tabPosts.map(post => (
-          <article key={post.id}>
-            <Title href={post.link} target="_blank">
-              {post.title}
-            </Title>
-            <Description>{post.desc}</Description>
-            <Author>{post.author}</Author>
-            <Votes>{post.votes}</Votes>
-            <HourDate>
-              {Math.round((Date.now() - post.date * 1000) / 1000 / 60 / 60)}{" "}
-              hours ago.
-            </HourDate>
-            <Comments>{post.comments}</Comments>
-            <Tags>{post.tags}</Tags>
-          </article>
-        ))}
-      </div>
+      <PostsWrapper>
+        TODAY
+        {tabPosts
+          .slice(0, this.state.showingAll ? tabPosts.length : 10)
+          .map(post => (
+            <Post key={post.id}>
+              <Title href={post.link} target="_blank">
+                {post.title}
+              </Title>
+              <Description>{post.desc}</Description>
+              <Author>{post.author}</Author>
+              <Votes>{post.votes}</Votes>
+              <HourDate>
+                {Math.round((Date.now() - post.date * 1000) / 1000 / 60 / 60)}{" "}
+                hours ago.
+              </HourDate>
+              <Comments>{post.comments}</Comments>
+              <Tags>{post.tags}</Tags>
+            </Post>
+          ))}
+        {tabPosts.length > 10 && !this.state.showingAll ? (
+          <ShowMore onClick={this.showMore}>
+            Show more({tabPosts.length - 10})
+          </ShowMore>
+        ) : null}
+        <DivBottomButton>
+          <BottomButton> PREVIOUS DAY </BottomButton>
+        </DivBottomButton>
+      </PostsWrapper>
     );
   }
 }
